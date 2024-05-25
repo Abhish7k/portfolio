e9c050fc21373f68ff92f5fde89d4daaf0cf62f7
Abhishek <abhishekbhosale.dev@gmail.com>
2024-05-26 02:44:29 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/Navbar";
+import { ThemeToggle } from "@/components/ThemeToggle";
+import Image from "next/image";
+
+export default function Home() {
+  return <main></main>;
+}
